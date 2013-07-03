25e284e4970819b6d304dc2d9ad5cd45c43daa3a
alexander fedorov <elf607@ya.ru>
2013-07-03 15:07:27 +0600

    making kinda progress in parsing config file

--- xdbc.sh
+++ xdbc.sh
@@ -2,8 +2,18 @@
 
 conf_file="xdbc.cfg.example";
 
+cat $conf_file | while read line 
+do
+# for line in "`cat $conf_file`" ; do
+	is_commented="`expr index "$line" '#'`";
+	# echo $is_commented;
+	# echo "${#line}";
+	if [ "$is_commented" != 1 -a "${#line}" != 0 ]; then
+		echo `expr match "$line" 'Domains'`;
+		#if `$line | grep "Domain"`
 
-for line in "`cat $conf_file`" ; do
-	echo "$line";
+		echo "$line";
+	fi
+	
 done
 
