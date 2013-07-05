87a727edd48bf7abf2c6e27c89ab846f373ac0c7
alexander fedorov <elf607@ya.ru>
2013-07-05 13:11:41 +0600

    working logging

--- xdbc.sh
+++ xdbc.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 conf_file="xdbc.cfg";
+backup_path="";
+log_file_name="test";
 
-
-cat $conf_file | while read line 
+while read line 
 do
 # for line in "`cat $conf_file`" ; do
 	is_commented="`expr index "$line" '#'`";
 	has_equal_sign="`expr index "$line" '='`";
-	log_file_name="test";
 	# echo $is_commented;
 	# echo "${#line}";
 	if [ "$is_commented" != 1 -a "${#line}" != 0 ]; then
 		if [ "$has_equal_sign" != 0 -a "`expr substr "$line" 1 "$(($has_equal_sign-1))"`" = "backup_path" ]; then
 			# echo "${line:0:"$has_equal_sign"}";
 
-			backup_path=`expr substr "$line" "$(($has_equal_sign+1))" "${#line}"`;
+			backup_path="`expr substr "$line" "$(($has_equal_sign+1))" "${#line}"`";
 			
 			echo "##########################################" >> "$backup_path/$log_file_name";
 
@@ -28,7 +28,7 @@ do
 		fi
 
 		if [ "$line" = "[Domains]" ]; then
-			echo "В этой строке определено начало перечисления доменов";
+			# echo "В этой строке определено начало перечисления доменов";
 
 		fi
 
@@ -53,12 +53,13 @@ do
 		
 		#if `$line | grep "Domain"`
 
-		echo "$line";
+		# echo "$line";
 	fi
 	
-done
+done < $conf_file 
 
-echo $log_file_name;
+# echo "$backup_path";
+# echo "$log_file_name";
 
 echo "------------------------------------------" >> "$backup_path/$log_file_name";
 
