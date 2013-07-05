d4cd7c3af51f8bc82280d25592dc06f6c2a85483
alexander fedorov <elf607@ya.ru>
2013-07-05 12:34:45 +0600

    dd file final message not working :(

--- xdbc.sh
+++ xdbc.sh
@@ -2,11 +2,13 @@
 
 conf_file="xdbc.cfg";
 
+
 cat $conf_file | while read line 
 do
 # for line in "`cat $conf_file`" ; do
 	is_commented="`expr index "$line" '#'`";
 	has_equal_sign="`expr index "$line" '='`";
+	log_file_name="test";
 	# echo $is_commented;
 	# echo "${#line}";
 	if [ "$is_commented" != 1 -a "${#line}" != 0 ]; then
@@ -15,9 +17,10 @@ do
 
 			backup_path=`expr substr "$line" "$(($has_equal_sign+1))" "${#line}"`;
 			
+			echo "##########################################" >> "$backup_path/$log_file_name";
 
 			log_message="Backup script started";
-			echo "["`date +%d.%m.%y\ %T`"] >>" "$log_message" >> "$backup_path/test";
+			echo "["`date +%d.%m.%y\ %T`"] >>" "$log_message" >> "$backup_path/$log_file_name";
 			
 			# echo `date +%d.%m.%y\ %T`;
 			# echo "$index";
@@ -26,6 +29,26 @@ do
 
 		if [ "$line" = "[Domains]" ]; then
 			echo "В этой строке определено начало перечисления доменов";
+
+		fi
+
+		has_delimeter="`expr index "$line" ';'`";
+
+		if [ "$has_delimeter" != 0 ]; then
+			domain_name="`expr substr "$line" 1 "$(($has_delimeter-1))"`"
+			domain_path="`expr substr "$line" "$(($has_delimeter+1))" "${#line}"`"
+
+			echo "------------------------------------------" >> "$backup_path/$log_file_name";
+
+			log_message="DD of <<$domain_name>> image started";
+		
+			echo "["`date +%d.%m.%y\ %T`"] >>" "$log_message" >> "$backup_path/$log_file_name";
+			
+			dd if="$domain_path" of="$backup_path/$domain_name.backup" bs=512M;
+
+			log_message="DD of <<$domain_name>> image ended";
+
+			echo "["`date +%d.%m.%y\ %T`"] >>" "$log_message" >> "$backup_path/$log_file_name";
 		fi
 		
 		#if `$line | grep "Domain"`
@@ -35,3 +58,9 @@ do
 	
 done
 
+echo $log_file_name;
+
+echo "------------------------------------------" >> "$backup_path/$log_file_name";
+
+log_message="Backup finished";
+echo "["`date +%d.%m.%y\ %T`"] >>" "$log_message" >> "$backup_path/$log_file_name";
